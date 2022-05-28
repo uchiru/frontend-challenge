c7c858f1a87a10e4e0994e9ef23ec5c567709450
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-05-28 20:38:54 +0300

    Add/Remove cats into/from favorites.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,9 @@
 const catsContainer = document.querySelector('.cats');
 
-const createFavoriteButton = () => {
+let cats = [];
+let favoriteCats = [];
+
+const createFavoriteButton = (cat) => {
   const favoriteButtonElement = document.createElement('div');
   favoriteButtonElement.classList.add('favorite-button');
   favoriteButtonElement.classList.add('non-favorite');
@@ -9,10 +12,12 @@ const createFavoriteButton = () => {
     if (favoriteButtonElement.classList.contains('non-favorite')) {
       favoriteButtonElement.classList.remove('non-favorite');
       favoriteButtonElement.classList.add('is-favorite');
+      addIntoFavorites(cat)
     }
     else if (favoriteButtonElement.classList.contains('is-favorite')) {
       favoriteButtonElement.classList.remove('is-favorite');
       favoriteButtonElement.classList.add('non-favorite');
+      removeFromFavorites(cat.id)
     }
   }
 
@@ -21,16 +26,27 @@ const createFavoriteButton = () => {
   return favoriteButtonElement;
 }
 
-const createCatCard = (image) => {
+function addIntoFavorites(cat) {
+  favoriteCats.push(cat);
+  console.log('favorite cats:', favoriteCats);
+}
+
+function removeFromFavorites(id) {
+  favoriteCats = favoriteCats.filter((cat) => cat.id !== id);
+  console.log('favorite cats:', favoriteCats);
+}
+
+const createCatCard = (cat) => {
   const cardElement = document.createElement('div');
   const catImageElement = document.createElement('img');
-  const favoriteButtonElement = createFavoriteButton();
+  const favoriteButtonElement = createFavoriteButton(cat);
   cardElement.classList.add('cat');
   cardElement.classList.add('shadow');
-  catImageElement.src = image;
+  catImageElement.src = cat.url;
   cardElement.appendChild(catImageElement);
   cardElement.appendChild(favoriteButtonElement);
   catsContainer.appendChild(cardElement);
 }
 
-createCatCard("https://cdn2.thecatapi.com/images/bn1.jpg");
+createCatCard({ id: 'ceh', url: "https://cdn2.thecatapi.com/images/bn1.jpg"});
+createCatCard({ id: 'nh', url: "https://cdn2.thecatapi.com/images/bn6.jpg"});
