e2a3836898cde3502d2ba25f6157b04c996180b0
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-05-29 09:35:41 +0300

    Use inner integer id instead of api id for slices.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Routes,
@@ -12,20 +12,35 @@ import Header from './Header';
 
 import './App.css';
 
+
+
 function App() {
-  const [cats, setCats] = useState([
-    { id: 1, url: 'https://cdn2.thecatapi.com/images/wIFSp4brt.jpg', isFavorite: false },
-    { id: 2, url: 'https://cdn2.thecatapi.com/images/2hk.jpg', isFavorite: false },
-    { id: 3, url: 'https://cdn2.thecatapi.com/images/bhm.jpg', isFavorite: false },
-    { id: 4, url: 'https://cdn2.thecatapi.com/images/t8oArUO-L.jpg', isFavorite: false },
-  ]);
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    async function fetchCats(page, limit) {
+      let baseURL = 'https://api.thecatapi.com/v1/images/search';
+      const headers = {
+        headers: {
+          'x-api-key': 'fe54a132-287b-4e6b-9eb1-a2174ae94c6d'
+        }
+      }
+      const requestedURL = baseURL + `?limit=${limit}&page=${page}&order=ASC`;
+      console.log(requestedURL);
+      const response = await fetch(requestedURL, headers);
+      let catsJSON = await response.json();
+      catsJSON = catsJSON.map((cat, index) => ({...cat, isFavorite: false, _id: index}));
+      setCats(catsJSON);
+    }
+    fetchCats(1, 15);
+  }, []);
 
   const toggleFavorites = (id) => {
     const cat = cats.find((cat) => cat.id === id);
 
     if (cat) {
       cat.isFavorite = !cat.isFavorite;
-      setCats([...cats.slice(0, cat.id - 1), cat, ...cats.slice(cat.id)])
+      setCats([...cats.slice(0, cat._id), cat, ...cats.slice(cat._id + 1)])
     }
   };
 
