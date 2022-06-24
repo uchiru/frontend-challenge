379b254822d5f5aa9b9cd909852cc7bbef4b68b4
kirberez <o6olgyu@gmail.com>
2022-06-24 13:10:16 +0300

    index.html inside public

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('js is ok')
 root.render(
   <React.StrictMode>
     <CookiesProvider>
