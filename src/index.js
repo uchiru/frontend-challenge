461a5970b20c5d81db1550cf1847e6a63ad907fd
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-01-30 17:02:06 +0300

    Started developing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'app';
 
+const $root = document.querySelector('#root');
+
 if ($root) {
   const root = createRoot($root);
 
@@ -12,4 +14,3 @@ if ($root) {
     </Provider>
   );
 };
-
