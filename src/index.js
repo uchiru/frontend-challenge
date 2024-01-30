b5aab20293f3f5ccb8670f1fb70338b4a8b685e3
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-01-30 16:41:13 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from 'app';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from 'app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if ($root) {
+  const root = createRoot($root);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+
