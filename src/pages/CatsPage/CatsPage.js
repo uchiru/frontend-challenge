70b085a5e66d2f51959544cc807fb518eeb00d9d
KoloskovaAlla <91485080+KoloskovaAlla@users.noreply.github.com>
2024-01-31 22:29:48 +0300

    Started to make styles for CatsPage

--- src/pages/CatsPage/CatsPage.js
+++ src/pages/CatsPage/CatsPage.js
@@ -1,47 +1,32 @@
-import { classes } from './CatsPage.module.scss';
+import classes from './CatsPage.module.scss';
 import { useState, useEffect } from 'react';
 import { Cat } from './components';
 
 export const CatsPage = () => {
-
   const [catsData, setCatsData] = useState();
-
-
-
-  // Функция для получения 10 фотографий кошек
-  const getCatImages = () => {
+  
+  const getCatsImages = () => {
     fetch("https://api.thecatapi.com/v1/images/search?limit=10") // Используем параметр limit=10 для получения 10 фотографий
-      .then(response => response.json()) // Преобразуем ответ в JSON формат
+      .then(response => response.json()) 
       .then(data => {
-        setCatsData(data);
-        // console.log(data); // Выводим данные в консоль
-        // Здесь вы можете обработать полученные данные, например, отобразить их на странице
+        setCatsData(data);       
       })
-      .catch(error => console.log('error', error)); // Обрабатываем возможные ошибки
+      .catch(error => console.log('error', error)); 
   };
 
-  // Вызываем функцию для получения фотографий кошек
   useEffect(() => {
-    getCatImages();
+    getCatsImages();
   }, []);
-
-  useEffect(() => {
-    console.log(catsData);
-  }, []);
-
 
   if (catsData) return (
     <main>
       Котики
-      <div className={classes.wrapper}>
-        {/* <div> */}
-        {/* <ul className={classes.list}> */}
-        <ul>
+      <div className={classes.wrapper}>        
+        <ul className={classes.list}>   
           {Object.values(catsData).map((cat, index) => (
             <li
-              // className={classes.listItem}
-
-              key={index}
+              className={classes.listItem}
+              key={index}            
             >
               <Cat cat={cat} />
             </li>
--- src/pages/CatsPage/components/Cat/Cat.js
+++ src/pages/CatsPage/components/Cat/Cat.js
@@ -1,11 +1,12 @@
-export const Cat = ({ cat }) => {
+import classes from './Cat.module.scss';
 
+export const Cat = ({ cat }) => {
   console.log(cat.url);
 
   return (
     // <div>{`Кот ${cat.id}`}</div>
     // <div className={className}>
-    <div>
+    <div className={classes.image}>
       <img
         src={cat?.url && cat.url}
         alt={'здесь был кот'}
