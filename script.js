fc734ba575b40218cbce86e8c107ef1a062ee026
Sofia <sophia.shashova@yandex.ru>
2024-02-02 14:43:48 +0300

    Add images in favourites by like

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const url = `https://api.thecatapi.com/v1/images/search?limit=100`;
 const src_heart = `img/favorite.svg`;
 const api_key = "live_6QkaEZSm5hKSRpvMVyBSRFebrHNCZE3RqKm8liTu6aONO2wTaeWW88uYkTylWHSr";
 
+
 const fetchCats = async () => {
   try {
     const response = await fetch(url, {headers: {"x-api-key": api_key,},});
@@ -13,7 +14,9 @@ const fetchCats = async () => {
   }
 };
 
+
 const CatsList = async () => {
+  try {
   const ImagesList = await fetchCats();
   ImagesList.map(function (imageList) {
     let image = document.createElement("img");
@@ -31,34 +34,58 @@ const CatsList = async () => {
     gridCell.appendChild(image);
     gridCell.appendChild(heart);
     
+    
 
     document.getElementById("grid").appendChild(gridCell);
   });
+}catch (error) {
+  console.log(error.message);
+}
 }
 
-const getFaveData = (elem) => {
-  const parent = elem.parentElement;
-  const img=parent.querySelector('img').src;
-}
+
+// const getFaveData = (elem) => {
+//   const parent = elem.parentElement;
+//   const img=parent.querySelector('img').src;
+// }
 
 const ListenForLikes = () => {
   const likes = document.querySelectorAll('.heart');
-  console.log(likes);
+ let LikedImgSrc = [];
+  let datalist;
   likes.forEach(like => {
-    console.log(like)
     like.addEventListener('click', (event) => {
       event.target.classList.toggle('like-no');
-      event.target.classList.toggle('like-yes');
+      event.target.classList.toggle('like-yes'); 
+      
       if (event.target.classList.contains('like-yes')) {
+
+        // let FavouriteWrapper = document.createElement("div");
+        // FavouriteWrapper.appendChild(like.parentElement);
+        // localStorage.setItem('test', FavouriteWrapper.appendChild(like.parentElement))
+        // console.log(localStorage)
+        // localStorage.getItem('test', FavouriteWrapper.appendChild(like.parentElement))
+        // console.log(localStorage)
+        //document.getElementById("image-wrapper").appendChild(FavouriteWrapper);
+      //   datalist=JSON.parse(localStorage.getItem('test'))
+      // LikedImgSrc=datalist;
+        LikedImgSrc.push(like.previousSibling.src);
+        localStorage.setItem('test', JSON.stringify(LikedImgSrc));
+        console.log(localStorage)
+         window.href="./favourites.html";
+        //console.log(like)
+        //console.log(like.previousSibling.src)
+        //  console.log(FavouriteWrapper)
+
         console.log(' сохранено');
         // var like = document.querySelector('.heart');
         // like.id = 'like-yes';
-        getFaveData(event.target);
+        // getFaveData(event.target);
       } else {
         // var like = document.querySelector('.heart');
         // like.id = 'like-no';
         console.log('removing')
-        getFaveData(event.target);
+        // getFaveData(event.target);
       }
     })
   })
