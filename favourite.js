08455e9f2065cd089874e08b3ff15685e5dd1da8
Sofia <sophia.shashova@yandex.ru>
2024-02-07 09:57:15 +0300

    Add deleting from main page

--- favourite.js
+++ favourite.js
@@ -1,16 +1,21 @@
+//const src_heart = `img/favorite_fill.svg`;
+
 let FavouritesAdding = async () => {
   const LikedImgSrc = await JSON.parse(localStorage.getItem("test"));
   console.log(LikedImgSrc);
   LikedImgSrc.forEach((el) => {
+    let heart = document.createElement("div");
     let image = document.createElement("img");
     image.src = `${el}`;
-    image.classList.add("img");
-
+    heart.src = src_heart;
+    image.classList.add("image");
+    heart.classList.add("like");
     let gridCell = document.createElement("div");
-      gridCell.classList.add("col");
-      gridCell.classList.add("col-lg");
-      gridCell.appendChild(image);
-      //gridCell.appendChild(heart);
+    gridCell.classList.add("col");
+    gridCell.classList.add("col-lg");
+    gridCell.appendChild(image);
+    gridCell.appendChild(heart);
+    //gridCell.appendChild(heart);
     document.getElementById("image-wrapper").appendChild(gridCell);
     // console.log(LikedImgSrc);
   });
@@ -18,4 +23,11 @@ let FavouritesAdding = async () => {
   // localStorage.setItem('test', JSON.stringify(LikedImgSrc));
   // localStorage.clear();
 };
-FavouritesAdding();
+
+let FavouritesDeleting = async () => {};
+
+const FavouritesPage = async () => {
+  FavouritesAdding();
+};
+
+FavouritesPage();
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-
 const url = `https://api.thecatapi.com/v1/images/search?limit=100`;
 const src_heart = `img/favorite.svg`;
 const api_key =
   "live_6QkaEZSm5hKSRpvMVyBSRFebrHNCZE3RqKm8liTu6aONO2wTaeWW88uYkTylWHSr";
+const container = document.getElementById("grid");
+const loadMore = document.getElementById("load-more");
+let start = 0;
+let limit = 15;
+let data = [];
 
 const fetchCats = async () => {
   try {
     const response = await fetch(url, { headers: { "x-api-key": api_key } });
     const data = await response.json();
-    let imagesData = sessionStorage.getItem("url") ? JSON.parse(sessionStorage.getItem("url")) : await data;
+    let imagesData = sessionStorage.getItem("url")
+      ? JSON.parse(sessionStorage.getItem("url"))
+      : await data;
     sessionStorage.setItem("url", JSON.stringify(imagesData));
     return imagesData;
   } catch (error) {
@@ -28,15 +34,17 @@ const CatsList = async () => {
       image.classList.add("image");
       heart.classList.add("heart");
       heart.classList.add("like-no");
+      image.classList.add("lazy"); //добавляем ленивую загрузку
 
       let gridCell = document.createElement("div");
+
       gridCell.classList.add("col");
       gridCell.classList.add("col-lg");
       gridCell.appendChild(image);
       gridCell.appendChild(heart);
-
       document.getElementById("grid").appendChild(gridCell);
     });
+    //data = ImagesList.map((el) => el.url)
   } catch (error) {
     console.log(error.message);
   }
@@ -49,33 +57,61 @@ const CatsList = async () => {
 
 const ListenForLikes = async () => {
   const likes = document.querySelectorAll(".heart");
-  likes.forEach((like) => {
+  likes.forEach(async (like) => {
     like.addEventListener("click", (event) => {
       event.target.classList.toggle("like-no");
       event.target.classList.toggle("like-yes");
-
+      let LikedImgSrc = localStorage.getItem("test")
+        ? JSON.parse(localStorage.getItem("test"))
+        : [];
       if (event.target.classList.contains("like-yes")) {
-        let LikedImgSrc = localStorage.getItem("test") ? JSON.parse(localStorage.getItem("test")) : [];
         LikedImgSrc.push(like.previousSibling.src);
         localStorage.setItem("test", JSON.stringify(LikedImgSrc));
-        window.href = "./favourites.html";
-        console.log(" сохранено");
+        //console.log(LikedImgSrc[0]);
+        console.log("сохранено");
+        console.log(LikedImgSrc);
         //localStorage.clear();
         // var like = document.querySelector('.heart');
         // getFaveData(event.target);
       } else {
-        // var like = document.querySelector('.heart');
+        LikedImgSrc = LikedImgSrc.filter((image) => image !== like.previousSibling.src);
+        localStorage.setItem("test", JSON.stringify(LikedImgSrc));
         console.log("removing");
+        console.log(LikedImgSrc);
+        //localStorage.removeItem("test", LikedImgSrc[LikedImgSrc.length - 1]);
+        // console.log(LikedImgSrc[LikedImgSrc.length - 1]);
+        // window.href = "./favourites.html";
+        // var like = document.querySelector('.heart');
         // getFaveData(event.target);
       }
     });
   });
 };
 
-const CatPinterest = async () => {
+const MainPage = async () => {
   const Run = await CatsList();
   Run;
   ListenForLikes();
 };
 
-CatPinterest();
+MainPage();
+
+// function renderData(data) {
+//   data.forEach(item => {
+//     const element=document.createElement('div');
+//     element.src = item;
+//     container.appendChild(element)
+//   })
+// }
+
+// function loadData(){
+//   const end = start + limit;
+//   const newData=data.slice(start, end);
+//   renderData(newData);
+//   start = end;
+//   if (start>=data.length){
+//     loadMore.style.display = 'none';
+//   }
+// }
+
+// loadMore.addEventListener('click', loadData)
