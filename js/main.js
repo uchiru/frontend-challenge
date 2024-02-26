aad649efd2a3f4edbaec914cfe0635c25ed18487
Vlad <vladgod@mail.ru>
2024-02-26 14:02:22 +0300

    Добавляет добавление/удаление любимых фото

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@ import './nav.js';
 import {getPhotos} from './api.js';
 
 //Определяем url по умолчанию
-<<<<<<< HEAD
 const url = `https://api.thecatapi.com/v1/images/search?limit=20`;
 
 //Выводим 20 случайных фото кошек
-=======
-const url = `https://api.thecatapi.com/v1/images/search?limit=10`;
-//Выводим 10 случайных фото кошек
->>>>>>> viewing
 getPhotos(url);
